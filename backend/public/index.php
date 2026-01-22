0bf6696e0dd2aafe67ca4f7fd492940ab1b148c9
Py12 <BaptisteJOYEZ>
2026-01-22 16:25:45 +0100

    Fix : index concours api url

--- backend/public/index.php
+++ backend/public/index.php
@@ -53,7 +53,7 @@ if ($uri === "api/submit/drawing" && $_SERVER["REQUEST_METHOD"] === "POST") {
 
 if ($uri === "api/get/concours" && $_SERVER["REQUEST_METHOD"] === "POST") {
     require_once "../src/models/Concour.php";
-    (new ConcourController())->getConcour();
+    (new ConcoursController())->getConcours();
     exit;
 }
 
